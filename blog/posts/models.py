31f184db7a8b527f98fa3664ded10cf3ead92fe8
pengoox <amaanabbasi99@gmail.com>
2018-08-03 21:07:09 +0530

    title commit

--- blog/posts/models.py
+++ blog/posts/models.py
@@ -1,22 +1,8 @@
 from django.db import models
 
 class Post(models.Model):
-
-    DJANGO_REST_FRAMEWORK = "DRF"
-    TENSOR_FLOW = "TF"
-    REACT_JS = "RJ"
-
-    ARTICLE_TYPE_CHOICES = (
-        (DJANGO_REST_FRAMEWORK, "Django Rest Framework"),
-        (TENSOR_FLOW, "Tensor Flow"),
-        (REACT_JS, "React JavaScript"),
-    )
-
+    
     title = models.CharField(max_length=120)
     content = models.TextField()
-    article_type = models.CharField('type', blank=True, max_length=3, choices=ARTICLE_TYPE_CHOICES)
     
-    class Meta:
-        verbose_name = "post"
-        verbose_name_plural = 'posts'
 
