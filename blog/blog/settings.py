1d46fed696f97c68e68017e4a92852d62e90ed53
pengoox <amaanabbasi99@gmail.com>
2018-08-09 23:45:03 +0530

    Added jwt authentication

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
-
+from datetime import timedelta
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -122,3 +122,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES' : ('rest_framework.permissions.IsAuthenticated',),
+    'DEFAULT_AUTHENTICATION_CLASSES': ('rest_framework_simplejwt.authentication.JWTAuthentication',),
+}
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+}
--- blog/posts/models.py
+++ blog/posts/models.py
@@ -4,5 +4,10 @@ class Post(models.Model):
     
     title = models.CharField(max_length=120)
     content = models.TextField()
+
+
+    def __str__(self):
+
+        return self.title
     
 
